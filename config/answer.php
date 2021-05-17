5ee324111db5a873811d11d5a23bbf92363c3279
Simone Romiti <sr1992ei@gmail.com>
2021-05-17 22:51:11 +0200

    Small changes

--- config/answer.php
+++ config/answer.php
@@ -1,228 +1,345 @@
 <?php 
 
    $answers = [
-      
-      [
-         'A' => [
-            'option' => 'Terzo',
-            'correct' => 1
-           
-         ],
-         
-         'B' => [
-            'option' => 'Quinto',
-            'correct' => 0
-         ],
-         
-         'C' => [
-            'option' => 'Secondo',
-            'correct' => 0
-         ],
-         
-         'D' => [
-            'option' => 'Primo',
-            'correct' => 0
-         ],
+
+   [
+      'A' => [
+         'option' => 'Terzo',
+         'correct' => 1
+
       ],
 
-
-      [
-         'A' => [
-            'option' => 'Con Leslie Winkle',
-            'correct' => 0
-         ],
-
-         'B' => [
-            'option' => 'Con una sua amica',
-            'correct' => 0
-         ],
-
-         'C' => [
-            'option' => 'Con sua cugina',
-            'correct' => 1
-         ],
-
-         'D' => [
-            'option' => 'Con una sconosciuta',
-            'correct' => 0
-         ],
-      ],
-      
-      [
-         'A' => [
-            'option' => 'Shutter Island',
-            'correct' => 0
-         ],
-         
-         'B' => [
-            'option' => 'Inception',
-            'correct' => 1
-         ],
-         
-         'C' => [
-            'option' => 'Interstellar',
-            'correct' => 0
-         ],
-         
-         'D' => [
-            'option' => 'The wolf of Wall Street',
-            'correct' => 0
-         ],
-      ],
-      [
-         'A' => [
-            'option' => 'Il Signore Degli Anelli',
-            'correct' => 0
-         ],
-         
-         'B' => [
-            'option' => 'Twilight',
-            'correct' => 0
-         ],
-         
-         'C' => [
-            'option' => '50 Sfumature di Grigio',
-            'correct' => 0
-         ],
-         
-         'D' => [
-            'option' => 'Harry Potter',
-            'correct' => 1
-         ],
-      ],
-      [
-         'A' => [
-            'option' => '7',
-            'correct' => 1
-         ],
-
-         'B' => [
-            'option' => '10',
-            'correct' => 0
-         ],
-
-         'C' => [
-            'option' => '5',
-            'correct' => 0
-         ],
-
-         'D' => [
-            'option' => '14',
-            'correct' => 0
-         ],
+      'B' => [
+         'option' => 'Quinto',
+         'correct' => 0
       ],
 
-      [
-         'A' => [
-            'option' => 1,
-            'correct' => 0
-         ],
-
-         'B' => [
-            'option' => 12,
-            'correct' => 0
-         ],
-
-         'C' => [
-            'option' => 700,
-            'correct' => 1
-         ],
-
-         'D' => [
-            'option' => 'Infiniti',
-            'correct' => 0
-         ],
+      'C' => [
+         'option' => 'Secondo',
+         'correct' => 0
       ],
 
-      [
-         'A' => [
-            'option' => 'Brad Pitt',
-            'correct' => 0
-         ],
+      'D' => [
+         'option' => 'Primo',
+         'correct' => 0
+      ],
+   ],
 
-         'B' => [
-            'option' => 'Tom Cruise',
-            'correct' => 1
-         ],
 
-         'C' => [
-            'option' => 'Leonardo Di Caprio',
-            'correct' => 0
-         ],
-
-         'D' => [
-            'option' => 'Jhonny Depp',
-            'correct' => 0
-         ],
+   [
+      'A' => [
+         'option' => 'Con Leslie Winkle',
+         'correct' => 0
       ],
 
-      [
-         'A' => [
-            'option' => 1997,
-            'correct' => 1
-         ],
-
-         'B' => [
-            'option' => 2001,
-            'correct' => 0
-         ],
-
-         'C' => [
-            'option' => 2009,
-            'correct' => 0
-         ],
-
-         'D' => [
-            'option' => 2015,
-            'correct' => 0
-         ],
+      'B' => [
+         'option' => 'Con una sua amica',
+         'correct' => 0
       ],
 
-      [
-         'A' => [
-            'option' => 'Kiyomi',
-            'correct' => 0
-         ],
+      'C' => [
+         'option' => 'Con sua cugina',
+         'correct' => 1
+      ],
 
-         'B' => [
-            'option' => 'Naomi',
-            'correct' => 0
-         ],
+      'D' => [
+         'option' => 'Con una sconosciuta',
+         'correct' => 0
+      ],
+   ],
 
-         'C' => [
-            'option' => 'Wedy',
-            'correct' => 0
-         ],
+   [
+      'A' => [
+         'option' => 'Shutter Island',
+         'correct' => 0
+      ],
 
-         'D' => [
+      'B' => [
+         'option' => 'Inception',
+         'correct' => 1
+      ],
+
+      'C' => [
+         'option' => 'Interstellar',
+         'correct' => 0
+      ],
+
+      'D' => [
+         'option' => 'The wolf of Wall Street',
+         'correct' => 0
+      ],
+   ],
+   [
+      'A' => [
+         'option' => 'Il Signore Degli Anelli',
+         'correct' => 0
+      ],
+
+      'B' => [
+         'option' => 'Twilight',
+         'correct' => 0
+      ],
+
+      'C' => [
+         'option' => '50 Sfumature di Grigio',
+         'correct' => 0
+      ],
+
+      'D' => [
+         'option' => 'Harry Potter',
+         'correct' => 1
+      ],
+   ],
+   [
+      'A' => [
+         'option' => '7',
+         'correct' => 1
+      ],
+
+      'B' => [
+         'option' => '10',
+         'correct' => 0
+      ],
+
+      'C' => [
+         'option' => '5',
+         'correct' => 0
+      ],
+
+      'D' => [
+         'option' => '14',
+         'correct' => 0
+      ],
+   ],
+
+   [
+      'A' => [
+         'option' => 1,
+         'correct' => 0
+      ],
+
+      'B' => [
+         'option' => 12,
+         'correct' => 0
+      ],
+
+      'C' => [
+         'option' => 700,
+         'correct' => 1
+      ],
+
+      'D' => [
+         'option' => 'Infiniti',
+         'correct' => 0
+      ],
+   ],
+
+   [
+      'A' => [
+         'option' => 'Brad Pitt',
+         'correct' => 0
+      ],
+
+      'B' => [
+         'option' => 'Tom Cruise',
+         'correct' => 1
+      ],
+
+      'C' => [
+         'option' => 'Leonardo Di Caprio',
+         'correct' => 0
+      ],
+
+      'D' => [
+         'option' => 'Jhonny Depp',
+         'correct' => 0
+      ],
+   ],
+
+   [
+      'A' => [
+         'option' => 1997,
+         'correct' => 1
+      ],
+
+      'B' => [
+         'option' => 2001,
+         'correct' => 0
+      ],
+
+      'C' => [
+         'option' => 2009,
+         'correct' => 0
+      ],
+
+      'D' => [
+         'option' => 2015,
+         'correct' => 0
+      ],
+   ],
+
+   [
+      'A' => [
+         'option' => 'Kiyomi',
+         'correct' => 0
+      ],
+
+      'B' => [
+         'option' => 'Naomi',
+         'correct' => 0
+      ],
+
+      'C' => [
+         'option' => 'Wedy',
+         'correct' => 0
+      ],
+
+      'D' => [
+
+         'option' => 'Sayu',
+         'correct' => 1
+      ],
+   ],
+
+   [
+      'A' => [
+         'option' => 'Dentro il mostro',
+         'correct' => 0
+      ],
+
+      'B' => [
+         'option' => 'Confusione',
+         'correct' => 0
+      ],
+
+      'C' => [
+         'option' => 'Il vero dilemma',
+         'correct' => 0
+      ],
+
+      'D' => [
+
+         'option' => 'Uno studio rosa',
+         'correct' => 1
+      ],
+   ],
+
+   // Secondo Set Domande
+      // [
+      //    'A' => [
+      //       'option' => 'Blue Monday',
+      //       'correct' => 1
+      //    ],
+
+      //    'B' => [
+      //       'option' => 'Big Wednesday',
+      //       'correct' => 0
+      //    ],
+
+      //    'C' => [
+      //       'option' => 'Black Friday',
+      //       'correct' => 0
+      //    ],
+
+      //    'D' => [
             
-            'option' => 'Sayu',
-            'correct' => 1
-         ],
-      ],
+      //       'option' => 'Stormy Monday.',
+      //       'correct' => 0
+      //    ],
+      // ],
 
-      [
-         'A' => [
-            'option' => 'Dentro il mostro',
-            'correct' => 0
-         ],
+      // [
+      //    'A' => [
+      //       'option' => 'Fiume Azzurro',
+      //       'correct' => 0
+      //    ],
 
-         'B' => [
-            'option' => 'Confusione',
-            'correct' => 0
-         ],
+      //    'B' => [
+      //       'option' => 'Rio delle Ammazzoni',
+      //       'correct' => 0
+      //    ],
 
-         'C' => [
-            'option' => 'Il vero dilemma',
-            'correct' => 0
-         ],
+      //    'C' => [
+      //       'option' => 'Danubio',
+      //       'correct' => 0
+      //    ],
 
-         'D' => [
-            
-            'option' => 'Uno studio rosa',
-            'correct' => 1
-         ],
-      ],
+      //    'D' => [
+
+      //       'option' => 'Nilo',
+      //       'correct' => 1
+      //    ],
+      // ],
+
+      // [
+      //    'A' => [
+      //       'option' => '5',
+      //       'correct' => 0
+      //    ],
+
+      //    'B' => [
+      //       'option' => '11',
+      //       'correct' => 1
+      //    ],
+
+      //    'C' => [
+      //       'option' => '8',
+      //       'correct' => 0
+      //    ],
+
+      //    'D' => [
+
+      //       'option' => '16',
+      //       'correct' => 0
+      //    ],
+      // ],
+
+      // [
+      //    'A' => [
+      //       'option' => "Pinkfong Kids 'Songs & Stories – Baby Shark Dance",
+      //       'correct' => 1
+      //    ],
+
+      //    'B' => [
+      //       'option' => 'Luis Fonsi ft. Daddy Yankee – Despacito',
+      //       'correct' => 0
+      //    ],
+
+      //    'C' => [
+      //       'option' => 'Ed Sheeran – Shape of You',
+      //       'correct' => 0
+      //    ],
+
+      //    'D' => [
+
+      //       'option' => 'PSY – Gangnam Style ',
+      //       'correct' => 0
+      //    ],
+      // ],
+
+      // [
+      //    'A' => [
+      //       'option' => "Sex Education",
+      //       'correct' => 1
+      //    ],
+
+      //    'B' => [
+      //       'option' => 'Dark',
+      //       'correct' => 0
+      //    ],
+
+      //    'C' => [
+      //       'option' => 'La Casa di carta',
+      //       'correct' => 0
+      //    ],
+
+      //    'D' => [
+
+      //       'option' => 'Stranger Things',
+      //       'correct' => 1
+      //    ],
+      // ],
+
 
 
    ];
--- config/question.php
+++ config/question.php
@@ -1,17 +1,27 @@
 <?php 
 
    $questions = [
-      'Se in una gara sorpasso il terzo, in che posizione mi trovo?',
-      'In The Big Bang Theory con chi perde la verginità Howard?',
-      'In quale film viene detta la seguente frase: "I sogni sembrano reali finché ci siamo dentro, non ti pare? Solo quando ci svegliamo ci rendiamo conto che c`era qualcosa di strano"',
-      'Quale è la serie di libri più venduti del XXI secolo?',
-      'Quanti episodi ha la prima stagione di Breaking Bad?',
-      'In Harry Potter quanti possibili falli di Quidditch ci sono?',
-      'Da quale attore poteva essere intepretato Iron Man a differenza di Robert Downey Jr.?',
-      'Quando è stata fondata Netflix?',
-      'In Death Note come si chiama la sorella di Light?',
-      'Come si intitola il primo episodio della serie Sherlock?',
+   // Primo Set domande
+   'Se in una gara sorpasso il terzo, in che posizione mi trovo?',
+   'In The Big Bang Theory con chi perde la verginità Howard?',
+   'In quale film viene detta la seguente frase: "I sogni sembrano reali finché ci siamo dentro, non ti pare? Solo quando ci svegliamo ci rendiamo conto che c`era qualcosa di strano"',
+   'Quale è la serie di libri più venduti del XXI secolo?',
+   'Quanti episodi ha la prima stagione di Breaking Bad?',
+   'In Harry Potter quanti possibili falli di Quidditch ci sono?',
+   'Da quale attore poteva essere intepretato Iron Man a differenza di Robert Downey Jr.?',
+   'Quando è stata fondata Netflix?',
+   'In Death Note come si chiama la sorella di Light?',
+   'Come si intitola il primo episodio della serie Sherlock?',
 
+   // // Secondo Set Domande
+   // "Qual'è il nome della caffetteria nella sitcom Friends?",
+   // 'L’espressione inglese che indica il giorno di gennaio ritenuto il più triste dell’anno è…',
+   // "Qual'è il fiume più lungo del mondo?",
+   // 'Quanti fusi orari ci sono in Russia?',
+   // "Qual'è il video YouTube con più visualizzazioni?",
+   // "Qual'è la serie Netflix più vista del 2019?"
+   
+   
    ];
 
    return $questions;
